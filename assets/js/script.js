4ce389199e7147930569844c83e1775cff76efb6
ryanp <rdpodols@gmail.com>
2022-03-18 12:17:33 -0400

    high score page added

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,6 +22,9 @@
     //when i hit the main menu button, it deletes the div/area that is holding the timer that appears and then shows you your score
     //it also sets the value of the timer back to 70 and indexes back to 0
 
+// find a way to store multiple high scores into a table
+
+
 
 
 var quiz = {
@@ -68,6 +71,7 @@ var infoEl = document.createElement("div");
 var startButton = document.querySelector("#start-game");
 var clearPage = document.querySelector("main-page");
 var questionArea = document.querySelector("quiz");
+var highScoreButton = document.querySelector("#score-page");
 
 // function to start the game
 function startGame() {
@@ -77,30 +81,38 @@ function startGame() {
 };
 
 function displayNextQuestion() {
-    var currentQuestion = quiz.questions[quiz.currentQuestionIndex];
+    var currentQuestion = getCurrentQuestion();
     var divEl = document.createElement("div");
-            divEl.setAttribute("id", "containerRemove");
-            document.getElementById("quiz").appendChild(divEl);
+    divEl.setAttribute("id", "containerRemove");
+    document.getElementById("quiz").appendChild(divEl);
+
     var h1El = document.createElement("h1");
-            h1El.textContent = currentQuestion['question'];
-            document.getElementById("containerRemove").appendChild(h1El); 
+    h1El.textContent = currentQuestion['question'];
+    document.getElementById("containerRemove").appendChild(h1El); 
+
     for (x = 0; x < currentQuestion['choices'].length; x++) {
         var div1 = document.createElement("div");
-            div1.setAttribute("id", "button-text-"+x.toString());
-            document.getElementById("containerRemove").appendChild(div1);
-        var labelEl = document.createElement("button");
-            labelEl.textContent = currentQuestion['choices'][x];
-            document.getElementById("button-text-"+x.toString()).appendChild(labelEl);
+        div1.setAttribute("id", "button-text-"+x.toString());
+        document.getElementById("containerRemove").appendChild(div1);
         
-            labelEl.addEventListener("click", checkAnswer);
+        var labelEl = document.createElement("button");
+        labelEl.textContent = currentQuestion['choices'][x];
+        labelEl.setAttribute("id", "answer-buttons");
+        document.getElementById("button-text-"+x.toString()).appendChild(labelEl);
+        labelEl.addEventListener("click", checkAnswer);
     }
 }
 
 var timer = 70;
 
+function getCurrentQuestion() {
+    return quiz.questions[quiz.currentQuestionIndex];
+}
+
 function countdown() {
     var timerH1 = document.createElement("h1");
     timerH1.innerText = timer + ' seconds remaining';
+
     var timerDiv1 = document.createElement("div");
     timerDiv1.setAttribute("id", "timerContainer");
     document.getElementById("timer").appendChild(timerDiv1);
@@ -119,9 +131,8 @@ function countdown() {
         } else if (gameTimeEnd = 1) {
             clearInterval(timeInterval);
             timerH1.innerHTML = "Your score is: " + timer;
-            
         } else {
-            timer = '';
+            timer == 0;
             clearInterval(timeInterval);
             timerH1.innerHTEML = "Your score is: " + timer;
         };
@@ -130,8 +141,128 @@ function countdown() {
 
 function resetGame () {
         var timer = 70;
-        //remove the old timer
-        //create new timer
+};
+
+function resetScreen() {
+    quiz.currentQuestionIndex++;
+    document.getElementById("containerRemove").remove();
+    if (quiz.currentQuestionIndex == quiz.questions.length) {        
+        console.log('endGame');
+        gameTimerEnd++;
+        endGame();
+    } else{
+        displayNextQuestion();
+    }
+}
+
+function newScreenReset() {
+    document.getElementById("containerRemove").remove();
+    quiz.currentQuestionIndex = 0;
+    gameTimerEnd = 0;
+    timer = 70;
+        displayNextQuestion();
+        countdown();
+};
+
+function clearScoreScreen () {
+    localStorage.clear();
+    newGame();
+};
+
+// function highScorePageCreation() {
+//     var highScoreLinkButton = document.createElement("button");
+//     highScoreLinkButton.textContent = "High Scores - Click Here!";
+//     highScoreLinkButton.setAttribute("id", "scorePageContainer");
+//     document.getElementById("high-scores-page").appendChild(highScoreLinkButton);
+
+//     highScoreLinkButton.addEventListener("click", scorePage);
+// };
+
+
+function scorePage () {
+    document.getElementById("main-page").remove();
+    //document.getElementById("scorePageContainer").remove();
+    //document.getElementById("deleteScorePage").remove();
+
+    var highScoreTitle = document.createElement("h1");
+    highScoreTitle.textContent = "High Scores:"
+    document.getElementById("high-scores-page").appendChild(highScoreTitle);
+
+    //add here where to pull the list of high scores - getItem local storage?
+    var myScoreData = localStorage.getItem(inputText);
+    console.log(myScoreData);
+
+    var highScoreInfo = document.createElement("div");
+    highScoreInfo.setAttribute("id", "scoreList");
+    document.getElementById("high-scores-page").appendChild("highScoreInfo");
+}
+
+function highScorePage() {
+    //highScorePageCreation();
+    document.getElementById("deleteScorePage").remove();
+    var endGameDiv2El = document.createElement("div");
+    endGameDiv2El.setAttribute("id", "endGame");
+    document.getElementById("quiz").appendChild(endGameDiv2El);
+
+    var clearScores = document.createElement("button");
+    clearScores.setAttribute("id", "scoreReset");
+    clearScores.textContent = "Clear High Scores";
+    document.getElementById("endGame").appendChild(clearScores);
+
+    clearScores.addEventListener("click", clearScoreScreen);
+
+    var mainMenu = document.createElement("button");
+    mainMenu.setAttribute("id", "mainMenuButton");
+    mainMenu.textContent = "Main Menu";
+    document.getElementById("endGame").appendChild(mainMenu);
+
+    var mainMenuButton = document.querySelector("#mainMenuButton");
+    mainMenuButton.addEventListener("click", newGame);
+
+};
+
+function newGame() {
+    //highScorePageCreation();
+    //div h1 p button
+    var timerContainerElement = document.getElementById("timerContainer");
+    if (timerContainerElement != null){
+        document.getElementById("timerContainer").remove();
+    };
+    var endGameElement = document.getElementById("endGame");
+    if (endGameElement != null) {
+        document.getElementById("endGame").remove();
+    };
+    var highScoreElement = document.getElementById("score-title-div");
+    if (highScoreElement != null) {
+        document.getElementById("score-title-div").remove();
+    };
+    
+
+    var menuDivEl = document.createElement("div");
+    menuDivEl.setAttribute("id", "containerRemove");
+    document.getElementById("quiz").appendChild(menuDivEl);
+
+    var h1NewGame = document.createElement("h1");
+    h1NewGame.textContent = "Coding Quiz Challenge";
+    document.getElementById("containerRemove").appendChild(h1NewGame);
+
+    var pNewGame = document.createElement("p");
+    pNewGame.textContent = "Welcome to the coding quiz challenge! Answer as many coding related questions you can before the time limit is up.  Wrong answers will deduct ten seconds from the time remaining.  Answer all questions before time is up for a chance at the new high score!";
+    document.getElementById("containerRemove").appendChild(pNewGame);
+        
+    var buttonNewGame = document.createElement("button");
+    buttonNewGame.textContent = "Start Quiz!";
+    buttonNewGame.setAttribute("id", "start-game2");
+    document.getElementById("containerRemove").appendChild(buttonNewGame);
+
+    var newStartButton = document.querySelector("#start-game2");
+    quiz.correctCount = 0;
+    quiz.currentQuestionIndex = 0;
+
+    /*timerStr = timer.toString();
+    localStorage.setItem("High Score", timerStr);*/
+    //var timer = 70;
+    newStartButton.addEventListener("click", newScreenReset);
 };
 
 function checkAnswer(event) {
@@ -156,105 +287,161 @@ function checkAnswer(event) {
             timer = timer - 10;
             resetScreen();
         }
-
-    function resetScreen() {
-        quiz.currentQuestionIndex++;
-        document.getElementById("containerRemove").remove();
-        if (quiz.currentQuestionIndex == quiz.questions.length) {        
-            console.log('endGame');
-            gameTimerEnd++;
-            endGame();
-        } else{
-            displayNextQuestion();
-        }
-    }
-
-    function newScreenReset() {
-            document.getElementById("containerRemove").remove();
-            quiz.currentQuestionIndex = 0;
-            gameTimerEnd = 0;
-            timer = 70;
-                displayNextQuestion();
-                countdown();
-    }
-
-    function endGame() {
-        var endGameDivEl = document.createElement("div");
-            endGameDivEl.setAttribute("id", "endGame");
-            document.getElementById("quiz").appendChild(endGameDivEl);
-        var endGameText = document.createElement("h1");
-            endGameText.textContent = "All Done!";
-            document.getElementById("endGame").appendChild(endGameText);
-        var finalScoreText = document.createElement("h1");
-            finalScoreText.textContent = "You scored " + quiz.correctCount + " points.";
-            document.getElementById("endGame").appendChild(finalScoreText);
-        var formName = document.createElement("form");
-            formName.setAttribute("id", "highScore");
-            document.getElementById("endGame").appendChild(formName);
-        var enterName = document.createElement("label");
-            enterName.textContent = "Your Initials: ";
-            document.getElementById("highScore").appendChild(enterName);
-        var inputName = document.createElement("input");
-            document.getElementById("highScore").appendChild(inputName);
-        var submitButton = document.createElement("button");
-            submitButton.textContent = "Submit";
-            document.getElementById("highScore").appendChild(submitButton);
-            
-            var submitButtonQ = document.querySelector("#highScore");
-            submitButtonQ.addEventListener("click", highScorePage);
-
-            function highScorePage() {
-                document.getElementById("endGame").remove();
-                var endGameDiv2El = document.createElement("div");
-                    endGameDiv2El.setAttribute("id", "endGame");
-                    document.getElementById("quiz").appendChild(endGameDiv2El);
-                var clearScores = document.createElement("button");
-                    clearScores.setAttribute("id", "scoreReset");
-                    clearScores.textContent = "Clear High Scores";
-                    document.getElementById("endGame").appendChild(clearScores);
-                var mainMenu = document.createElement("button");
-                    mainMenu.setAttribute("id", "mainMenuButton");
-                    mainMenu.textContent = "Main Menu";
-                    document.getElementById("endGame").appendChild(mainMenu);
-        
-                var mainMenuButton = document.querySelector("#mainMenuButton");
-                mainMenuButton.addEventListener("click", newGame);
-        
-            };
-
-            function newGame() {
-                //div h1 p button
-                document.getElementById("timerContainer").remove();
-                document.getElementById("endGame").remove();
-                var menuDivEl = document.createElement("div");
-                    menuDivEl.setAttribute("id", "containerRemove");
-                    document.getElementById("quiz").appendChild(menuDivEl);
-                var h1NewGame = document.createElement("h1");
-                    h1NewGame.textContent = "Coding Quiz Challenge";
-                    document.getElementById("containerRemove").appendChild(h1NewGame);
-                var pNewGame = document.createElement("p");
-                    pNewGame.textContent = "Welcome to the coding quiz challenge! Answer as many coding related questions you can before the time limit is up.  Wrong answers will deduct ten seconds from the time remaining.  Answer all questions before time is up for a chance at the new high score!";
-                    document.getElementById("containerRemove").appendChild(pNewGame);
-                var buttonNewGame = document.createElement("button");
-                    buttonNewGame.textContent = "Start Quiz!";
-                    buttonNewGame.setAttribute("id", "start-game2");
-                    document.getElementById("containerRemove").appendChild(buttonNewGame);
-                
-                var newStartButton = document.querySelector("#start-game2");
-                quiz.correctCount = 0;
-                quiz.currentQuestionIndex = 0;
-
-                timerStr = timer.toString();
-                localStorage.setItem("High Score", timerStr);
-                //var timer = 70;
-                newStartButton.addEventListener("click", newScreenReset);
-            };
-};
 };
 
+function endGame() {
+    createHtmlContainerForEndScreen();
+    appendEndGameTextToEndScreen();
+    //appendFinalScoreText();
+    createEndGameForm();
+};
+
+function saveScore() {
+    var quizHighScores = JSON.parse(localStorage.getItem("quizHighScores")) ?? [];
+
+    var name = document.getElementById("highScoreName").value; 
+    var newScore = timer.toString();
+
+    var highScoreObject = {
+        name: name,
+        score: newScore
+    };
+
+    quizHighScores.push(highScoreObject);
+
+    var sortedHighScores = quizHighScores.sort((a, b) => b.score - a.score);
+
+    var topTenScores = sortedHighScores.slice(0, 10);
+
+    localStorage.setItem("quizHighScores", JSON.stringify(topTenScores));
+
+    highScorePage();
+}
 
 // click the start button
 startButton.addEventListener("click", startGame);
+highScoreButton.addEventListener("click", showHighScores);
 
-//function that resets timer from the main page
-//start game actually goes to a new function, and that new function starts the timer and thetn takes you into the game, so that when you do a new game, it goes into that new function of timer reset/into game
+function createEndGameForm() {
+    //highScorePageCreation();
+    var formName = document.createElement("form");
+    formName.setAttribute("id", "highScore");
+    //document.getElementById("endGame").appendChild(formName);
+    document.body.appendChild(formName);
+
+    // name input label
+    var enterName = document.createElement("label");
+    enterName.setAttribute("id", "deleteScorePage");
+    enterName.textContent = "Your Initials: ";
+    document.getElementById("highScore").appendChild(enterName);
+
+    // name input element
+    var inputName = document.createElement("input");
+    inputName.setAttribute("id", "highScoreName");
+    document.getElementById("deleteScorePage").appendChild(inputName);
+
+    // input button element
+    var submitButton = document.createElement("button");
+    submitButton.setAttribute("id", "submitScore");
+    submitButton.textContent = "Submit";
+    document.getElementById("deleteScorePage").appendChild(submitButton);
+    var clickSave = document.getElementById("submitScore");
+    //document.getElementById("highScore").appendChild(submitButton);
+
+    clickSave.addEventListener("click", saveScore);
+
+    //var submitButtonQ = document.querySelector("#highScore");
+    //submitButtonQ.addEventListener("click", highScorePage);
+}
+
+function appendFinalScoreText() {
+    var finalScoreText = document.createElement("h1");
+    finalScoreText.textContent = "You scored " + quiz.correctCount + " points.";
+    document.getElementById("endGame").appendChild(finalScoreText);
+}
+
+function appendEndGameTextToEndScreen() {
+    var endGameText = document.createElement("h1");
+    endGameText.textContent = "All Done!";
+    document.getElementById("endGame").appendChild(endGameText);
+}
+
+function createHtmlContainerForEndScreen() {
+    var endGameDivEl = document.createElement("div");
+    endGameDivEl.setAttribute("id", "endGame");
+    document.getElementById("quiz").appendChild(endGameDivEl);
+}
+
+
+
+
+// can either have it always keep the 1 high score, or look up how to print top 3-5 scores, and evaluate if new score is higher each time
+
+//add if/then statements so if the question is right/wrong, it then shows it on the following screen under the next question
+
+//make a link on all pages that will take you to high scores - maybe only on main menu?
+
+//css to make it pretty
+
+var highScoreNumber = 10;
+var highScoreList = 'scoresHigh';
+var highScoreString = localStorage.getItem(highScoreList);
+var scoresHigh = JSON.parse(highScoreString) ?? [];
+var lowestScore = scoresHigh[highScoreNumber - 1]?.score ?? 0;
+
+function checkHighScore(score) {
+    var scoresHigh = JSON.parse(localStorage.getItem(highScoreString)) ?? []; 
+    var lowestScore = scoresHigh[highScoreNumber - 1]?.score ?? 0;
+
+    if (score > lowestScore) {
+        saveHighScore(score, scoresHigh);
+        showHighScores();
+    }
+}
+
+
+
+
+function showHighScores() {
+    var highScorePageElement = document.getElementById("score-title-div");
+    if (highScorePageElement != null) {
+        return;
+    }
+    var mainMenuElement = document.getElementById("main-page");
+    if (mainMenuElement != null) {
+        mainMenuElement.remove();
+    };
+    var containerRemoveElement = document.getElementById("containerRemove");
+    if (containerRemoveElement != null) {
+        containerRemoveElement.remove();
+    };
+
+    var scoreTitleDiv = document.createElement("div");
+    scoreTitleDiv.setAttribute("id", "score-title-div");
+    document.getElementById("quiz").appendChild(scoreTitleDiv);
+
+    var scoreTitleH1 = document.createElement("h1");
+    scoreTitleH1.textContent = "High Scores: ";
+    document.getElementById("score-title-div").appendChild(scoreTitleH1);
+
+    var scoreMenuButton = document.createElement("button");
+    scoreMenuButton.setAttribute("id", "score-menu-button");
+    scoreMenuButton.textContent = "Main Menu";
+    scoreMenuButton.addEventListener("click", newGame);
+    document.getElementById("score-title-div").appendChild(scoreMenuButton);
+    
+
+    var scoreOrList = document.createElement("ol");
+    scoreOrList.setAttribute("id", "scoresHigh");
+    document.getElementById("score-title-div").appendChild(scoreOrList);
+
+    var scoresHigh = JSON.parse(localStorage.getItem("quizHighScores")) ?? [];
+    var scoresHighList = document.getElementById(highScoreList);
+
+    scoresHighList.innerHTML = scoresHigh
+        .map((score) => `<li>${score.score} - ${score.name}`)
+        .join('');
+    
+        
+    //var scoreMenu = document.querySelector("score-menu-button");    
+}
